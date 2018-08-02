23afce1be50a1b335603db8429f43c04db8b1047
brooklynLife <uglyducklin@naver.com>
2018-08-02 09:27:58 +0900

    Update brooklyn.py

--- brooklyn.py
+++ brooklyn.py
@@ -1,13 +1,41 @@
 from gensim.models import FastText, KeyedVectors
 import random
 import re
+import nltk
+
 
 def similar(self):
-    #1. import a pretrained model
+    global model
     model = KeyedVectors.load_word2vec_format('./data/wiki-news/wiki-brooklyn.bin', binary=True, unicode_errors='ignore')
-    #print(model)
-    sim1 = model.most_similar(self, topn = 25)
-    # sim2 = '\n'.join([s[0] for s in sim1])
-    return sim1
+#    sim2 = []
+#    sim1 = model.most_similar(self, topn = 25)
+#    sim3 = [sim2.append(word) for word, score in sim1]
 
-# print(similar('apple'))
+############## KEYWORD EXTRACTION ##################
+    token = nltk.word_tokenize(self)
+    pos = nltk.pos_tag(token)
+
+    keyword_list = []
+    mostRelated = []
+
+    for s in pos:
+        if s[1] in ['NNP', 'JJ', 'NN']:
+            keyword_list.append(s[0])
+
+############## SIMILAR WORDS ##################
+
+
+    for i in keyword_list:
+        if i in model.vocab:
+            mostRelated_with_score = model.most_similar(positive=i, topn=10)
+            for word, score in mostRelated_with_score:
+                word2 = re.sub('[-=.#/?:$}]', '', word)
+                mostRelated.append(word2)
+
+    return mostRelated
+
+
+############## MARKOV_CHAIN ##################
+
+
+# def markov(self):
